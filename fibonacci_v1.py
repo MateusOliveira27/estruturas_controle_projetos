67b24d7b32364abe6150ab772f9fe2ce9e84773d
MateusOlivera27 <mateusoliveira13k@gmail.com>
2020-09-14 09:44:36 -0500

    fibonacci_v6.py

--- fibonacci_v1.py
+++ fibonacci_v1.py
@@ -1,11 +1,14 @@
 #!/usr/local/bin/python3
-def fibonacci(limite):
+def fibonacci(quantidade):
     resultado = [0, 1]
-    while resultado[-1] < limite:
+    while True:
         resultado.append(sum(resultado[-2:]))
+        if len(resultado) == quantidade:
+            break
     return resultado
 
 
 if __name__ == '__main__':
-    for fib in fibonacci(20000):
+    # Listar os 20 primeiros números da sequência
+    for fib in fibonacci(20):
         print(fib)
