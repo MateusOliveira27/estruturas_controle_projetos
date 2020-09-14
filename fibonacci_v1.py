aa5bb2d96caf8119193ea56fb57a026201759bb4
MateusOlivera27 <mateusoliveira13k@gmail.com>
2020-09-14 10:33:47 -0500

    fibonacci_v9.py

--- fibonacci_v1.py
+++ fibonacci_v1.py
@@ -1,9 +1,8 @@
 #!/usr/local/bin/python3
 def fibonacci(quantidade, sequencia=(0, 1)):
-    #Importante: Condição de parada
-    if len(sequencia) == quantidade:
-        return sequencia
-    return fibonacci(quantidade, sequencia + (sum(sequencia[-2:]),))
+    # Importante: Condição de parada
+    return sequencia if len(sequencia) == quantidade else \
+        fibonacci(quantidade, sequencia + (sum(sequencia[-2:]),))
 
 
 if __name__ == '__main__':
