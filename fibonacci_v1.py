0482b43dd3874f4e8e74919a68237b692a08b5f8
MateusOlivera27 <mateusoliveira13k@gmail.com>
2020-09-14 09:38:27 -0500

    fibonacci_v5.py

--- fibonacci_v1.py
+++ fibonacci_v1.py
@@ -2,7 +2,7 @@
 def fibonacci(limite):
     resultado = [0, 1]
     while resultado[-1] < limite:
-        resultado.append(resultado[-2] + resultado[-1])
+        resultado.append(sum(resultado[-2:]))
     return resultado
 
 
